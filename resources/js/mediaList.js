3e888daac6e81eacbac9eef1dfd42528e0130911
eaCe <webmaster@slaps.de>
2023-05-01 17:06:01 +0200

    fix: initial media list values [skip ci]

--- resources/js/mediaList.js
+++ resources/js/mediaList.js
@@ -1,13 +1,29 @@
 window.flexibleMediaList = (id, field) => {
   return {
     $element: null,
+    $select: null,
     init () {
       setTimeout(() => {
         this.$element = document.getElementById('REX_MEDIALIST_' + id);
+        this.$select = document.getElementById('REX_MEDIALIST_SELECT_' + id);
+
+        if (field.value !== '') {
+          this.setInitialValues();
+        }
       }, 1);
     },
+    setInitialValues () {
+      const values = field.value.split(',');
+      const options = [];
+
+      for (let i = 0; i < values.length; i++) {
+        options.push(`<option value="${values[i]}">${values[i]}</option>`);
+      }
+
+      this.$select.innerHTML = options.join('');
+      this.$element.value = field.value;
+    },
     setValue () {
-      // x-model not working for hidden inputs...
       field.value = this.$element.value;
     },
     updateValues () {
