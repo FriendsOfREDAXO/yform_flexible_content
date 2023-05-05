5afadde4367985127156353ab7845a0a64a4a60a
eaCe <webmaster@slaps.de>
2023-05-05 07:01:03 +0200

    chore: disable drag for now... [skip ci]

--- fragments/yform_flexible_content/output/group.php
+++ fragments/yform_flexible_content/output/group.php
@@ -5,25 +5,15 @@ $field = $field->parse('yform_flexible_content/output/field.php');
 
 <div class="my-5" x-cloak x-show="hasContent">
     <template x-for="(group, groupIndex) in groups" :key="group.groupId">
-        <div class="my-5 p-5 panel panel-edit transition-all flexible-group"
-             x-data="flexibleGroup($el)"
-             :class="{
-                'opacity-50': dragging
-             }"
-             @dragstart="dragStart($event)"
-             @dragend="dragEnd($event)"
-             @dragover="dragOver($event)"
-             @dragenter="dragEnter($event)"
-             @dragleave="dragLeave($event)"
-             @drop="drop($event)">
+        <div class="my-5 p-5 panel panel-edit transition-all flexible-group">
             <div class="flex form-group items-center justify-between">
                 <div class="flex items-center leading-none">
-                    <div class="pr-3 cursor-grab opacity-50 hover:opacity-100 transition-opacity"
-                         draggable="true">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M7 2a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                        </svg>
-                    </div>
+<!--                    <div class="pr-3 cursor-grab opacity-50 hover:opacity-100 transition-opacity"-->
+<!--                         draggable="true">-->
+<!--                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">-->
+<!--                            <path d="M7 2a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>-->
+<!--                        </svg>-->
+<!--                    </div>-->
 
                     <h3 x-text="getGroupName(group.id)" class="mt-0 mb-0 group-name"></h3>
                 </div>
--- resources/js/group.js
+++ resources/js/group.js
@@ -1,75 +1,74 @@
 window.flexibleGroup = ($element) => {
-    return {
-        dragging: false,
-        dragTarget: null,
-        $ghostElement: '',
-        dragStart(event) {
-            this.dragging = true;
+  return {
+    dragging: false,
+    dragTarget: null,
+    $ghostElement: '',
+    dragStart (event) {
+      this.dragging = true;
 
-            this.$ghostElement = document.createElement('div');
-            this.$ghostElement.classList.add('absolute', 'z-50', 'p-3', 'min-w-[300px]', 'bg-white', 'shadow-lg', 'panel', 'panel-edit', 'font-bold');
-            this.$ghostElement.innerHTML = $element.querySelector('.group-name').textContent;
+      this.$ghostElement = document.createElement('div');
+      this.$ghostElement.classList.add('absolute', 'z-50', 'p-3', 'min-w-[300px]', 'bg-white', 'shadow-lg', 'panel', 'panel-edit', 'font-bold');
+      this.$ghostElement.innerHTML = $element.querySelector('.group-name').textContent;
 
-            document.getElementById('flexible-drag-ghost').appendChild(this.$ghostElement);
-            event.dataTransfer.setDragImage(this.$ghostElement, 0, 0);
-            event.dataTransfer.effectAllowed = 'move';
-            event.dataTransfer.setData('text/plain', this.groupIndex);
-        },
-        dragOver(event) {
-            event.preventDefault();
-            event.stopPropagation();
+      document.getElementById('flexible-drag-ghost').appendChild(this.$ghostElement);
+      event.dataTransfer.setDragImage(this.$ghostElement, 0, 0);
+      event.dataTransfer.effectAllowed = 'move';
+      event.dataTransfer.setData('text/plain', this.groupIndex);
+    },
+    dragOver (event) {
+      event.preventDefault();
+      event.stopPropagation();
 
-            if (this.isSame(event.dataTransfer.getData('text/plain'))) {
-                return;
-            }
+      if (this.isSame(event.dataTransfer.getData('text/plain'))) {
+        return;
+      }
 
-            event.dataTransfer.dropEffect = 'move';
-            $element.classList.add('scale-105');
-            return false;
-        },
-        dragEnter(event) {
-            if (this.isSame(event.dataTransfer.getData('text/plain'))) {
-                return;
-            }
+      event.dataTransfer.dropEffect = 'move';
+      $element.classList.add('scale-105');
+      return false;
+    },
+    dragEnter (event) {
+      if (this.isSame(event.dataTransfer.getData('text/plain'))) {
+        return;
+      }
 
-            this.dragTarget = event.target;
-            $element.classList.add('scale-105');
-        },
-        dragLeave(event) {
-            if (this.dragTarget === event.target) {
-                event.stopPropagation();
-                event.preventDefault();
-                $element.classList.remove('scale-105');
-            }
-        },
-        drop(event) {
-            event.stopPropagation();
-            $element.classList.remove('scale-105');
-            const index = event.dataTransfer.getData('text/plain');
+      this.dragTarget = event.target;
+      $element.classList.add('scale-105');
+    },
+    dragLeave (event) {
+      if (this.dragTarget === event.target) {
+        event.stopPropagation();
+        event.preventDefault();
+        $element.classList.remove('scale-105');
+      }
+    },
+    drop (event) {
+      event.stopPropagation();
+      $element.classList.remove('scale-105');
+      const index = event.dataTransfer.getData('text/plain');
 
-            if (this.isSame(index)) {
-                return;
-            }
+      if (this.isSame(index)) {
+        return;
+      }
 
+      this.move(parseInt(index), this.groupIndex);
 
-            this.move(parseInt(index), this.groupIndex);
-
-            return false;
-        },
-        dragEnd(event) {
-            this.dragging = false;
-            $element.classList.remove('scale-105');
-            this.$ghostElement.remove();
-        },
-        parents($element, selector) {
-            const parents = [];
-            while (($element = $element.parentNode) && $element !== document) {
-                if (!selector || $element.matches(selector)) parents.push($element);
-            }
-            return parents;
-        },
-        isSame(index) {
-            return index == this.groupIndex;
-        }
+      return false;
+    },
+    dragEnd (event) {
+      this.dragging = false;
+      $element.classList.remove('scale-105');
+      this.$ghostElement.remove();
+    },
+    parents ($element, selector) {
+      const parents = [];
+      while (($element = $element.parentNode) && $element !== document) {
+        if (!selector || $element.matches(selector)) parents.push($element);
+      }
+      return parents;
+    },
+    isSame (index) {
+      return index == this.groupIndex;
     }
+  };
 };
