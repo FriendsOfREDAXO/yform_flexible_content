5c3fc100ac229820235e88d3faecf8f4461a1326
eaCe <webmaster@slaps.de>
2023-06-23 21:15:50 +0200

    WIP: test rexstan again :)

--- install.php
+++ install.php
@@ -3,16 +3,16 @@
 /** @var \rex_addon $this */
 
 $extensions = [
-    '..\..\..\..\redaxo\src\addons\rexstan\config\rex-superglobals.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\vendor\phpstan\phpstan\conf\bleedingEdge.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\vendor\phpstan\phpstan-strict-rules\rules.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\vendor\phpstan\phpstan-deprecation-rules\rules.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\config\phpstan-phpunit.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\config\phpstan-dba.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\config\cognitive-complexity.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\config\code-complexity.neon',
-    '..\..\..\..\redaxo\src\addons\rexstan\config\dead-code.neon'
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\config\rex-superglobals.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\vendor\phpstan\phpstan\conf\bleedingEdge.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\vendor\phpstan\phpstan-strict-rules\rules.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\vendor\phpstan\phpstan-deprecation-rules\rules.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\config\phpstan-phpunit.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\config\phpstan-dba.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\config\cognitive-complexity.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\config\code-complexity.neon',
+    '..\..\..\..\redaxo_cms\redaxo\src\addons\rexstan\config\dead-code.neon'
 ];
-$addon = ['..\..\..\..\redaxo\src\addons\yform_flexible_content'];
+$addon = ['..\..\..\..\redaxo_cms\redaxo\src\addons\yform_flexible_content'];
 
 \rexstan\RexStanUserConfig::save(9, $addon, $extensions, 80115);
