1a299cbbaa81b6ad2a07c625f027b663df2be06c
eaCe <webmaster@slaps.de>
2023-06-23 22:11:44 +0200

    it should work :(

--- .tools/rexstan.php
+++ .tools/rexstan.php
@@ -5,9 +5,10 @@ $REX['HTDOCS_PATH'] = '../../../../';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 $REX['LOAD_PAGE'] = false;
 
-require '../../../../core/boot.php';
-require '../../../../core/packages.php';
-
+require __DIR__ . '../../../../core/boot.php';
+require __DIR__ . '../../../../core/packages.php';
+dump(realpath(__DIR__ . '../../../../core/'));
+dump(realpath(__DIR__ . '../../../../core/packages.php'));
 $extensions = [
     '../../../../redaxo/src/addons/rexstan/config/rex-superglobals.neon',
     '../../../../redaxo/src/addons/rexstan/vendor/phpstan/phpstan/conf/bleedingEdge.neon',
